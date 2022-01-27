926b0d8e19428402802fef19eda06a47d268a004
antonio <antonio.evangelista@student.uniroma2.eu>
2022-01-27 19:25:45 +0100

    prova di leapfrog

--- solver.f90
+++ solver.f90
@@ -3,10 +3,62 @@ module solver
   implicit none
 
   public :: metropolis
-  public :: evolution
+  public :: leapfrog
 
 contains
 
+  subroutine simplex_2(u, v, dt, mul)
+    real(dp), intent(in) :: u(:)
+    real(dp), intent(in) :: v(:)
+    real(dp), allocatable :: u_i(:) 
+    real(dp), allocatable :: u_f(:)
+    real(dp), allocatable :: v_i(:)
+    real(dp), allocatable :: v_f(:)
+    real(dp) :: grad_s1 !la derivata del potenziale, quindi dell'azione(devo ancora definire sia azione che sua derivata)
+    real(dp) :: grad_s2
+    real(dp) :: grad_t
+    real(dp) :: dt
+    real(dp) :: mul
+    integer :: dim
+
+    dim=size(u)
+    allocate(u_i(dim))
+    allocate(u_f(dim))
+    allocate(v_i(dim))
+    allocate(v_f(dim))  
+
+    dt= mul*dt
+
+    u_i=u
+    v_i=v
+    
+    grad_s1=call(grad_s(u_i))
+    u_f=u_i + dt*call(grad_t(v_f -0.5_dp*dt*grad_s1))
+    grad_s2=call(grad_s(u_f))
+    v_f=v_i - 0.5_dp*dt*(grad_s1+grad_s2)
+
+  end subroutine simplex_2
+  
+  
+  !l'idea è di implementare un integratore simplettico del quardo ordine
+  subroutine leapfrog(u, v, tau, n)  
+    real(dp), intent(in) :: u(:) !prende il ruolo del campo
+    real(dp), intent(in) :: v(:) !prende il ruolo dell'impulso associato al campo
+    real(dp), allocatable :: u_1(:) 
+    real(dp), allocatable :: v_1(:)
+    real(dp), allocatable :: u_2(:) 
+    real(dp), allocatable :: v_2(:)
+    real(dp) :: dt !passo di integrazione nel tempo di evoluzione del microstato: n*dt=tau
+    real(dp) :: tau
+    integer :: n,i
+
+    dt=(tau/n)_dp
+    do i=0, n-1
+      !devo applicare tre volte volte simplex_2
+    end do
+    
+  end subroutine leapfrog
+  
   subroutine metropolis(phi_i, pi_i)
     real(dp), intent(inout) :: H_i
     real(dp), intent(inout) :: H_f
@@ -22,12 +74,12 @@ contains
 
     n=size(phi)
     phi_i=phi
-    allocate(pi_i)
+    pi_i=pi
     allocate(phi_f(n))
     allocate(pi_f(n))
 
-    phi_f =call(evolution(phi_i, pi_i,1))
-    pi_f  =call(evolution(phi_i, pi_i,2))
+    phi_f =call(leapfrog(phi_i, pi_i,1))
+    pi_f  =call(leapfrog(phi_i, pi_i,2))
     H_i   =call(H(phi_i,pi_i))
     H_f   =call(H(phi_f,pi_f))
     delta_H =H_f-H_i
