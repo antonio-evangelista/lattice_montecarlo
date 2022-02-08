fb81831b580f31f7c5b8dc5086ac338dace461ac
antonio-evangelista <98169151+antonio-evangelista@users.noreply.github.com>
2022-02-08 17:38:36 +0100

    introdotto cambiamenti sull'algoritmo

--- solver.f90
+++ solver.f90
@@ -8,15 +8,14 @@ module solver
 contains
 
   subroutine simplex_2(u, v, dt, mul)
-    real(dp), intent(in) :: u(:)
-    real(dp), intent(in) :: v(:)
-    real(dp), allocatable :: u_i(:) 
-    real(dp), allocatable :: u_f(:)
-    real(dp), allocatable :: v_i(:)
-    real(dp), allocatable :: v_f(:)
+    real(dp), intent(in) :: u(:,:,:) !campo
+    real(dp), intent(in) :: v(:,:,:) !impulso fittizio
+    real(dp), allocatable :: u_i(:,:,:) 
+    real(dp), intent(out) :: u_f(:,:,:)
+    real(dp), allocatable :: v_i(:,:,:)
+    real(dp), intent(out) :: v_f(:,:,:)
     real(dp) :: grad_s1 !la derivata del potenziale, quindi dell'azione(devo ancora definire sia azione che sua derivata)
     real(dp) :: grad_s2
-    real(dp) :: grad_t
     real(dp) :: dt
     real(dp) :: mul
     integer :: dim
@@ -33,7 +32,7 @@ contains
     v_i=v
     
     grad_s1=call(grad_s(u_i))
-    u_f=u_i + dt*call(grad_t(v_f -0.5_dp*dt*grad_s1))
+    u_f=u_i + dt*(v_f -0.5_dp*dt*grad_s1)
     grad_s2=call(grad_s(u_f))
     v_f=v_i - 0.5_dp*dt*(grad_s1+grad_s2)
 
@@ -41,33 +40,32 @@ contains
   
   
   !l'idea è di implementare un integratore simplettico del quardo ordine
-  subroutine leapfrog(u, v, tau, n)  
-    real(dp), intent(in) :: u(:) !prende il ruolo del campo
-    real(dp), intent(in) :: v(:) !prende il ruolo dell'impulso associato al campo
-    real(dp), allocatable :: u_1(:) 
-    real(dp), allocatable :: v_1(:)
-    real(dp), allocatable :: u_2(:) 
-    real(dp), allocatable :: v_2(:)
+  subroutine leapfrog(u, v, tau, n)  !devo capire come selzione u o v
+    real(dp), intent(inout) :: u(:) !prende il ruolo del campo
+    real(dp), intent(inout) :: v(:) !prende il ruolo dell'impulso associato al campo
     real(dp) :: dt !passo di integrazione nel tempo di evoluzione del microstato: n*dt=tau
     real(dp) :: tau
-    integer :: n,i
+    integer :: n,i,j
 
     dt=(tau/n)_dp
     do i=0, n-1
-      !devo applicare tre volte volte simplex_2
+      do j=0,2
+        u(:,:,:)=call(simplex_2(u,v,alpha))
+        v(:,:,:)=call(simplex_2(u,v,alpha))
+      end do
     end do
     
   end subroutine leapfrog
   
-  subroutine metropolis(phi_i, pi_i)
-    real(dp), intent(inout) :: H_i
-    real(dp), intent(inout) :: H_f
-    real(dp)                :: delta_H
-    real(dp), allocatable   :: phi_i(:)
-    real(dp), allocatable   :: pi_i(:)
-    real(dp), allocatable   :: phi_f(:)
-    real(dp), allocatable   :: pi_f(:)
-    real(dp), allocatable   :: phi(:)
+  subroutine metropolis(phi, pi)
+    real(dp) :: H_i
+    real(dp) :: H_f
+    real(dp) :: delta_H
+    real(dp), allocatable   :: phi_i(:,:,:)
+    real(dp), allocatable   :: pi_i(:,:,:)
+    real(dp), allocatable   :: phi_f(:,:,:)
+    real(dp), allocatable   :: pi_f(:,:,:)
+    real(dp), allocatable   :: phi(:,:,:)
     real(dp)  :: prob
     real(dp)  :: r
     integer   :: n
