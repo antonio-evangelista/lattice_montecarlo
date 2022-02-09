bca9b2f51e9ec15ed59c040ce894c97e2fbb8447
antonio <antonio.evangelista@student.uniroma2.eu>
2022-02-09 11:23:56 +0100

    Struttura generale del programma inserita

--- main.f90
+++ main.f90
@@ -4,9 +4,31 @@ program main
 
   implicit none
 
-  real(dp) :: phi(:,:,:)
-  real(dp) :: lambda
-  real(dp) :: m2
+  real(dp), allocatable :: phi(:,:,:) ! campo scalare reale in 2+1 dimensioni
+  real(dp), allocatable :: pi(:,:,:)  ! impulso fittizio per la dinamica del montecarlo
+  real(dp) :: lambda     ! coupling dimensionale dell'interazione
+  real(dp) :: m2      ! massa quadra dello scalare
+  real(dp) :: k       ! parametro di massa adimensionale
+  real(dp) :: g       ! coupling adimensionale
+  integer  :: L       ! parametro di discretizzazione del reticolo
+  integer  :: N       ! numero di step della catena di Markov (indicativamente credo sui 10^5)
+  real(dp) :: a       ! passo reticolare lo prendo uguale sia nella direzione spaziale che temporale
+  integer  :: i
+  allocate(phi(L,L,L))
+  allocate(pi(L,L,L))
+
+  call random_number(phi)
+ 
+  do i=1, N
+     call(init_pi(L,pi)) ! inizializzo l'impulso,forse non serve allocare pi all'interno di init_pi
+     call(metropolis(phi,pi))
+
+     ! per calcolare il correlatore devo capire quando arriva a termalizzazione
+     ! una volta raggiunta la termalizzazione sommo: sum_x phi(x)phi(0)esp(-S(phi))
+  end do
+  
+  
+  
 
 
 end program main
